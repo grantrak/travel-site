69ebe5f1470b0dee384803b1c7b904e003a6c37b
Grant <grantrak94@gmail.com>
2020-12-03 21:21:39 -0700

    completed modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,9 +3,25 @@ import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 
+
 let revealOnScroll = new RevealOnScroll();
-let mobileMenu = new MobileMenu();
-let stickyHeader = new StickyHeader();
+new MobileMenu();
+new StickyHeader();
+let modal
+
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+        modal = new x.default()
+        setTimeout(() => modal.openTheModal(), 20)
+      }).catch(() => console.log("There was a problem."))
+    } else {
+      modal.openTheModal()
+    }
+  })
+})
 
 
 if (module.hot) {
