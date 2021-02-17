d3f939d57174475afa833d06e9830f8907471122
Grant <grantrak94@gmail.com>
2021-02-17 09:09:04 -0700

    Customized the style and content of the site

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -31,9 +31,9 @@ class Modal {
         document.body.insertAdjacentHTML('beforeend', `
         <div class="modal">
         <div class="modal__inner">
-          <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg" class="section-title__icon"> Get in <strong>Touch</strong></h2>
+          <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail2.svg" class="section-title__icon"> Get in <strong>Touch</strong></h2>
           <div class="wrapper wrapper--narrow">
-            <p class="modal__description">We will have an online order system in place soon. Until then, connect with us on any of the platforms below!</p>
+            <p class="modal__description">We will have an online booking system in place soon. Until then, connect with us on any of the platforms below!</p>
           </div>
     
           <div class="social-icons">
